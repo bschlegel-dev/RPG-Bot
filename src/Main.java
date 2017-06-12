95fa73c1ca584810b7fe8b3d8dfcd3f1f8eca482
Ben Schlegel <scorer505@gmail.com>
2017-06-12 19:19:59 +0200

    Basic Bot added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,36 @@
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.events.IListener;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 
 public class Main {
+
+    public static void main(String[] args){
+
+        if(args.length != 1){
+            System.out.println("Please enter the bots token as the first argument e.g java -jar thisjar.jar tokenhere");
+            return;
+        }
+
+        IDiscordClient cli = BotUtils.getBuiltDiscordClient(args[0]);
+
+        
+        // Commented out as you don't really want duplicate listeners unless you're intentionally writing your code 
+        // like that.
+        // Register a listener via the IListener interface
+        cli.getDispatcher().registerListener(new IListener<MessageReceivedEvent>() {
+            public void handle(MessageReceivedEvent event) {
+                if(event.getMessage().getContent().startsWith(BotUtils.BOT_PREFIX + "test"))
+                    BotUtils.sendMessage(event.getChannel(), "I am sending a message from an IListener listener");
+            }
+        });
+        
+
+        // Register a listener via the EventSubscriber annotation which allows for organisation and delegation of events
+        cli.getDispatcher().registerListener(new MyEvents());
+
+        // Only login after all events are registered otherwise some may be missed.
+        cli.login();
+
+    }
 
 }
