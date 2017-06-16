c125c6046437092768e893a5d34c38c0b57e9b5c
Ben Schlegel <scorer505@gmail.com>
2017-06-16 17:57:30 +0200

    Cleaned up Commands, using dynamic arrays instead of static

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,9 @@ import sx.blah.discord.handle.impl.events.guild.channel.message.reaction.Reactio
 import sx.blah.discord.util.RequestBuffer;
 
 public class Main {
-	final static int cmdAmount = 5;
+	final static int cmdAmount = 6;
 	public static IDiscordClient cli;	
-	public static Command[] cmd = new Command[cmdAmount];	
+	public static Command[] cmd = new Command[0];	
 	public static IGuild iG;
 	public static void main(String[] args){		
 		if(args.length != 1){
@@ -29,10 +29,51 @@ public class Main {
 		cli = BotUtils.getBuiltDiscordClient(args[0]);		
 		cli.login();
 		
-		//Initialising Commands
+		//Initialising Commands		
 		Commands c = new Commands();
+		System.out.println("\n\n\n"+cmd.length);
+		for(int i = 0; i < cmd.length; i++) {
+			System.out.println(cmd[i].getName());
+		}
 		Request r = new Request();		
 		Help h = new Help();	
+	}
+	
+	public static void addCmd(Command newCmd) {
+		if(cmd.length == 0) {
+			cmd = new Command[1];
+			cmd[0] = newCmd;
+		}else {
+			boolean full = false;
+			int tempInt = 0;
+			
+			for(int i = 0; i < cmd.length; i++) {
+				if(cmd[i] != null) {
+					tempInt++;
+				}
+			}
+			
+			if(tempInt == cmd.length) {
+				full = true;
+			}
+			
+			if(!full) {
+				for(int i = 0; i < cmd.length; i++) {
+					if(cmd[i] != null) {
+						cmd[i] = newCmd;
+					}
+				}
+			}else {				
+				Command[] newcmd = new Command[cmd.length+1];				
+				for(int i = 0; i < cmd.length; i++) {
+					newcmd[i] = cmd[i];
+				}
+				newcmd[newcmd.length-1] = newCmd;
+				
+				cmd = newcmd;
+			}
+		}
+		CommandRegistry.getForClient(cli).register(newCmd);
 	}
 
 }
--- src/Request.java
+++ src/Request.java
@@ -58,7 +58,6 @@ public class Request {
 					}
 				});				
 			}
-		});		
-		CommandRegistry.getForClient(Main.cli).register(Main.cmd[1]);
+		});				
 	}
 }
