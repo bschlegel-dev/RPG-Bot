70f2c874c5899ed7b71b7cabb1e9ff20df7fd9ab
Ben Schlegel <scorer505@gmail.com>
2017-06-16 21:22:44 +0200

    Get and set Commands dynamically

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import org.json.simple.JSONObject;
+
 import com.darichey.discord.api.Command;
 import com.darichey.discord.api.CommandRegistry;
 import com.vdurmont.emoji.EmojiManager;
@@ -20,6 +22,7 @@ public class Main {
 	public static IDiscordClient cli;	
 	public static Command[] cmd = new Command[0];	
 	public static IGuild iG;
+	
 	public static void main(String[] args){		
 		if(args.length != 1){
 			System.out.println("Please enter the bots token as the first argument e.g java -jar thisjar.jar tokenhere");
@@ -30,15 +33,30 @@ public class Main {
 		cli.login();
 		
 		//Initialising Commands		
-		Commands c = new Commands();
-		System.out.println("\n\n\n"+cmd.length);
-		for(int i = 0; i < cmd.length; i++) {
-			System.out.println(cmd[i].getName());
-		}
+		Commands c = new Commands();		
 		Request r = new Request();		
-		Help h = new Help();	
+		Help h = new Help();		
 	}
 	
+	
+	/**
+	 * Gets a command from the Command array
+	 * @param name the name of the command
+	 * @return the Command with the name of the parameter (null if not found)
+	 */
+	public static Command getCommand(String name) {
+		for(int i = 0; i < cmd.length; i++) {
+			if(cmd[i].getName().equalsIgnoreCase(name)) {
+				return cmd[i];
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Adds a Command to the command Array and dynamically increases the arrays size if there is not enough
+	 * @param newCmd the command that gets added to the Array (appends it at the end of the array)
+	 */
 	public static void addCmd(Command newCmd) {
 		if(cmd.length == 0) {
 			cmd = new Command[1];
