6342882f0be7864adb9cf053a3a658d59de6abb0
Ben Schlegel <scorer505@gmail.com>
2017-06-20 08:14:42 +0200

    Added prefix to JSON file and reading it at start

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.darichey.discord.api.Command;
 import com.darichey.discord.api.CommandRegistry;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.vdurmont.emoji.EmojiManager;
 
 import sx.blah.discord.api.IDiscordClient;
@@ -22,23 +30,37 @@ public class Main {
 	public static IDiscordClient cli;	
 	public static Command[] cmd = new Command[0];	
 	public static IGuild iG;
-	
+
 	public static void main(String[] args){		
 		if(args.length != 1){
 			System.out.println("Please enter the bots token as the first argument e.g java -jar thisjar.jar tokenhere");
 			return;
 		}			
 		//Initialising Client
-		cli = BotUtils.getBuiltDiscordClient(args[0]);		
-		cli.login();
+		cli = BotUtils.getBuiltDiscordClient(args[0]);
+		cli.login();			
+
+		 JSONParser parser = new JSONParser();
+    	 try {
+    	 Object obj = parser.parse(new FileReader("test.json"));
+
+         JSONObject jsonObject = (JSONObject) obj;         
+         BotUtils.BOT_PREFIX = (String) jsonObject.get("bot_prefix");      
+         CommandRegistry.getForClient(Main.cli).setPrefix(BotUtils.BOT_PREFIX);
+    	 } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         } catch (IOException e) {
+             e.printStackTrace();
+         } catch (ParseException e) {
+             e.printStackTrace();
+         }
 		
 		//Initialising Commands		
 		Commands c = new Commands();		
 		Request r = new Request();		
 		Help h = new Help();		
 	}
-	
-	
+
 	/**
 	 * Gets a command from the Command array
 	 * @param name the name of the command
@@ -52,7 +74,7 @@ public class Main {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Adds a Command to the command Array and dynamically increases the arrays size if there is not enough
 	 * @param newCmd the command that gets added to the Array (appends it at the end of the array)
@@ -64,17 +86,17 @@ public class Main {
 		}else {
 			boolean full = false;
 			int tempInt = 0;
-			
+
 			for(int i = 0; i < cmd.length; i++) {
 				if(cmd[i] != null) {
 					tempInt++;
 				}
 			}
-			
+
 			if(tempInt == cmd.length) {
 				full = true;
 			}
-			
+
 			if(!full) {
 				for(int i = 0; i < cmd.length; i++) {
 					if(cmd[i] != null) {
@@ -87,7 +109,7 @@ public class Main {
 					newcmd[i] = cmd[i];
 				}
 				newcmd[newcmd.length-1] = newCmd;
-				
+
 				cmd = newcmd;
 			}
 		}
